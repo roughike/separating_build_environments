c6317496e4f5b86317676881273f4e95e9c88992
Iiro Krankka <iiro.krankka@codemate.com>
2018-02-27 00:07:50 +0200

    Renaming the main.dart to my_app.dart.

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:separating_build_environments/app_config.dart';
-import 'package:separating_build_environments/main.dart';
+import 'package:separating_build_environments/my_app.dart';
 
 void main() {
   var configuredApp = new AppConfig(
